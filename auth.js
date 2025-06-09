01325cc0d45d976f836294cf5aa2c55b650abfc6
Ajibare <ajibare.jide@gmail.com>
2025-06-09 15:54:27 +0100

    added  auth

--- auth.js
+++ auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const session = require('express-session');
 const express = require('jsonwebtoken')
-const bodyPaser = require('body-parser');
+// const bodyPaser = require('body-parser');
 const bodyParser = require('body-parser');
+const nodemailer = reqiure('nodemailer')
 
 
 const app = express();
 app.use(bodyParser.json())
+
 
 // Middleware to set up session management
 app.use(session({
@@ -65,6 +67,40 @@ app.get('/dashboard', (req, res) =>{
       res.send('token missing');
      }
 })
+
+
+const users = {}; // In-memory storage for demo purpose
+
+
+app.post('/request-access', (req, res) =>{
+  const {email} = req.body;
+
+  // Generate a 6-digit verification code
+const code = math.floor(10000 + Math.random() * 900000).toString();
+
+// Store the code in memory (users object)
+users[email] = code
+
+// simulated email sending (for demostration)
+console.log(`sending code ${code} to ${email}`)
+
+res.send('Code sent to your email')
+})
+
+app.post('/verify-code', (req, res) =>{
+  const {email, code} = req.body;
+
+  // Compare the received code with stored code doe the email
+  if(users[email] === code){
+    // code  match, access granted
+    res.send('Access granted')
+  } else{
+    // Code does not match, access denied
+    res.send('Invalid code')
+  }
+})
+
+
 
 // Start the server on poer 3000
 app.listen(3000, ()=> console.log('Server is running on port 3000'))
